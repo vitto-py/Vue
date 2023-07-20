4e1b45d9433aded10b41c683d56dcd92a60d9de6
Vitto Fiorito <germanyvitto@gmail.com>
2023-07-20 16:02:14 +0200

    The most basic Vue

--- Vue/01app.js
+++ Vue/01app.js
@@ -1,1 +1,14 @@
 console.log("new file")
+
+const app = Vue.createApp({
+    // data, function
+    //template: '<h2>I am the template</h2>'
+    data() {
+        return {
+            title: 'I am the template',
+            author: 'loquita',
+            age: 34
+        }
+    }
+})
+app.mount('#app')
