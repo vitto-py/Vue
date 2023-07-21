21dd22421e9a700a79b0838d0381c31e79707aea
Vitto Fiorito <germanyvitto@gmail.com>
2023-07-21 12:14:10 +0200

    new project

---
+++ Vue/02 The Vue CLI & Bigger Projects/01app.js
@@ -0,0 +1,20 @@
+console.log("new file")
+
+const app = Vue.createApp({
+    // data, function
+    //template: '<h2>I am the template</h2>'
+    data() {
+        return {
+            title: 'I am the template',
+            author: 'loquita',
+            age: 34
+        }
+    },
+    methods: {
+        changeAutor(a) {
+            //this.author = 'Dovstoyesky'
+            this.author = a
+        }
+    } 
+})
+app.mount('#app')
