8f147f5ed9e5cab9331b64ff7993d8b9ae1adc5e
Vittorio Fiorito <vitto9311@gmail.com>
2023-08-30 17:59:57 +0200

    Link new Component

---
+++ crash/01 CLI TaskTracker/tasktracker/src/components/Header.vue
@@ -0,0 +1,25 @@
+
+<template>
+  <header>
+    <h1>Some Text</h1>
+  </header>  
+</template>
+
+<script>
+/* vamos a enviar este Component a App.vue */
+/* Um componente contiene una pieza terminada: html, JS y CSS */
+
+/* Tenes que exportarlo como ves aqui */
+export default {
+  name: 'Header'
+}
+</script>
+
+<style scoped>
+  header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 20px;
+  }
+</style>
